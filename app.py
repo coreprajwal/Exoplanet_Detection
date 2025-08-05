8e2484230d95859964877ae8c341b9108afb9e0f
coreprajwal <prajwalpathade2003@gmail.com>
2025-08-05 16:48:38 +0530

    changes done required for app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.routes import predict
+from routes import predict
 import uvicorn
 
 app=FastAPI()
@@ -8,4 +8,4 @@ app.include_router(predict.router,prefix="/predict")
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
--- routes/predict.py
+++ routes/predict.py
@@ -1,7 +1,7 @@
 ## Adding Routes
 from fastapi import APIRouter
-from app.schema.predict import PredictResponse,PredictRequest
-from app.services.predict_logic import predict_service
+from schema.predict import PredictResponse,PredictRequest
+from services.predict_logic import predict_service
 
 router=APIRouter()
 
