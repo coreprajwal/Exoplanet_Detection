aa360db6314f3226679687977ac27e1243ef1ac0
coreprajwal <prajwalpathade2003@gmail.com>
2025-08-05 17:50:10 +0530

    uptime monitoring--requirement

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ app=FastAPI()
 
 app.include_router(predict.router,prefix="/predict")
 
+@app.get("/")
+def root():
+    return { "message": "API is Live", "usage": "Send a POST request to this route with the required JSON body."}
 
 if __name__ == "__main__":
     uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
